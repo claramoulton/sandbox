4627ea0f29bb4d0b56d9dc4c9d44fecac87861f6
Clara Moulton <claramoulton@Claras-MBP.lan>
2023-11-16 08:49:25 -0700

    first commit

---
+++ froyo_workshop/index.js
@@ -0,0 +1,5 @@
+const froyoFlavor1 = vanilla;
+const froyoFlavor2 = strawberry;
+const froyoFlavor3 = coffee;
+
+alert("Please enter which froyo flavors you would like to order, separated by a comma.")
---
+++ random_repo/script.js
@@ -0,0 +1,40 @@
+function countSevens(arr){
+    let sevens = 0;
+    for(let i = 0; i < arr.length; i = i + 1){
+        if(arr[i] === 7){
+            sevens = sevens +1;
+        }
+    }
+    return sevens;
+}
+
+function createRandoms(limit){
+    const numbers = {};
+    return numbers;
+    while(numbers.length < limit){
+        const num = Math.ceil(Math.random()*10);
+        numbers.push(num);
+    }
+}
+
+const numbers = [];
+
+while(numbers.length < 100){
+    const num = Math.ceil(Math.random()*10);
+    numbers.push(num);
+}
+
+/*
+let sevens = 0;
+for(let i = 0; i < numbers.length; i = i + 1){
+    if(numbers[i] === 7){
+        sevens = sevens + 1
+    }
+}
+
+console.log(sevens);
+*/
+
+console.log(createRandoms(3));
+//console.log(countSevens([]));
+//console.log(countSevens(numbers));
